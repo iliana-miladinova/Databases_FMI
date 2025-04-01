faa8291a29c34f5bf6ec1a82cfaedbfcb889b2ef
iliana-miladinova <iliana.miladinova@abv.bg>
2025-04-01 17:12:28 +0300

    Fixes

--- K1 PREP.sql
+++ K1 PREP.sql
@@ -15,7 +15,13 @@
 --				FROM pc);
 
 --9. Напишете заявка, която извежда средната цена на персоналните компютри за
---всяка честота по-голяма от 800.--SELECT SPEED, AVG(PRICE) AS AVG_SPEED--FROM pc--GROUP BY speed--HAVING speed > 800;--10.Напишете заявка, която извежда средния размер на диска на тези персонални
+--всяка честота по-голяма от 800.
+--SELECT SPEED, AVG(PRICE) AS AVG_SPEED
+--FROM pc
+--GROUP BY speed
+--HAVING speed > 800;
+
+--10.Напишете заявка, която извежда средния размер на диска на тези персонални
 --компютри, произведени от производители, които произвеждат и принтери.
 --Резултатът да се изведе за всеки отделен производител.
 
@@ -35,8 +41,31 @@
 --GROUP BY CLASS;
 
 --6. Напишете заявка, която извежда броя на корабите, потънали в битка според
---класа, за тези класове с повече от 2 кораба.--SELECT COUNT(SHIP), SHIPS.CLASS--FROM OUTCOMES INNER JOIN SHIPS--ON OUTCOMES.SHIP = SHIPS.NAME--WHERE RESULT = 'sunk' AND CLASS IN (SELECT CLASS--									FROM SHIPS--									GROUP BY CLASS--									HAVING COUNT(CLASS) > 2)--GROUP BY SHIPS.CLASS;--7. Напишете заявка, която извежда средния калибър на оръдията на корабите за
---всяка страна.--SELECT COUNTRY, AVG(BORE) AS AVG_BORE--FROM CLASSES INNER JOIN SHIPS--ON SHIPS.CLASS=CLASSES.CLASS--GROUP BY COUNTRY;--USE movies;--6. Напишете заявка, която извежда името на най-младия актьор (мъж).--SELECT TOP 1 NAME--FROM MOVIESTAR--WHERE GENDER = 'M'--ORDER BY BIRTHDATE DESC;--7. Напишете заявка, която извежда име на актьор и име на студио за тези
+--класа, за тези класове с повече от 2 кораба.
+--SELECT COUNT(SHIP), SHIPS.CLASS
+--FROM OUTCOMES INNER JOIN SHIPS
+--ON OUTCOMES.SHIP = SHIPS.NAME
+--WHERE RESULT = 'sunk' AND CLASS IN (SELECT CLASS
+--									FROM SHIPS
+--									GROUP BY CLASS
+--									HAVING COUNT(CLASS) > 2)
+--GROUP BY SHIPS.CLASS;
+
+--7. Напишете заявка, която извежда средния калибър на оръдията на корабите за
+--всяка страна.
+--SELECT COUNTRY, AVG(BORE) AS AVG_BORE
+--FROM CLASSES INNER JOIN SHIPS
+--ON SHIPS.CLASS=CLASSES.CLASS
+--GROUP BY COUNTRY;
+
+--USE movies;
+--6. Напишете заявка, която извежда името на най-младия актьор (мъж).
+--SELECT TOP 1 NAME
+--FROM MOVIESTAR
+--WHERE GENDER = 'M'
+--ORDER BY BIRTHDATE DESC;
+
+--7. Напишете заявка, която извежда име на актьор и име на студио за тези
 --актьори, участвали в най-много филми на това студио.
 SELECT DISTINCT STARNAME, STUDIONAME
 FROM STARSIN INNER JOIN MOVIE AS M
